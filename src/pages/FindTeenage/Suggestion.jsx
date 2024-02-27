bb0cf834020404e121423cc08704afd371ad7453
pierre <pierre@ND.fr>
2024-02-27 22:46:11 +0100

    wip

--- src/pages/FindTeenage/Suggestion.jsx
+++ src/pages/FindTeenage/Suggestion.jsx
@@ -4,16 +4,16 @@ import { useNavigate } from "react-router-dom";
 
 const Suggestion = ({ s, i }) => {
   const nodeRef = useRef(null);
-  const [enter, setEnter] = useState(false);
+  // const [enter, setEnter] = useState(false);
   const navigate = useNavigate();
 
   // useEffect(() => {
   //   setTimeout(() => {
   //     setEnter(true);
-  //   }, (i + 1) * 75);
+  //   }, (i + 1) * 200);
   // }, []);
   // if (!enter) {
-  //   return <div className="replace">test</div>;
+  //   return null;
   // }
   return (
     <div
@@ -22,11 +22,10 @@ const Suggestion = ({ s, i }) => {
       onClick={() => {
         navigate("/profil", { state: { teenId: s.id } });
       }}
-      data-aos={"fade-down"}
-      // data-aos={i % 2 ? "fade-left" : "fade-right"}
-      data-aos-duration="800"
-      data-aos-delay={i * 50}
-      data-aos-easing="ease-out-cubic"
+      // data-aos={"fade-down"}
+      // data-aos-duration="800"
+      // data-aos-delay={i * 50}
+      // data-aos-easing="ease-out-cubic"
     >
       {s.photo ? (
         <img src={s.photo} />
